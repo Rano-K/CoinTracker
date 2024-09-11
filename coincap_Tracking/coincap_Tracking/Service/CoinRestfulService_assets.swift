c7445e70948b499529c0e8d438c7f8a0bf0a9106
Rano-K <kmsoo0704@gmail.com>
2024-09-11 19:49:21 +0900

    CoinRestfulService 에 함수 오타 수정
    quoto -> quote

--- coincap_Tracking/coincap_Tracking/Service/CoinRestfulService_assets.swift
+++ coincap_Tracking/coincap_Tracking/Service/CoinRestfulService_assets.swift
@@ -2,16 +2,14 @@
 //  CoinRestfulService_assets.swift
 //  coincap_Tracking
 //
-//  Created by ms k on 8/30/24.
+//  Created by ms k on 9/11/24.
 //
 
-import SwiftUI
+import Foundation
 
-class CoinRestfulService_assets{
-    //api주소
-    private let  coinStoreUrl = "https://api.coincap.io/v2/assets/"
+class CoinRestfulService_assets {
+    private let coinStoreUrl = "https://api.coincap.io/v2/assets/"
     
-    //모든 코인정보 가져오기
     func quoteAllCoins(limit: Int = 0) async -> [Coin]? {
         
         var queryUrl = coinStoreUrl
@@ -20,45 +18,43 @@ class CoinRestfulService_assets{
             queryUrl += "?limit=\(limit)"
         }
         
-        guard let url = URL(string: queryUrl) else{
+        guard let url = URL(string: queryUrl) else {
             return nil
         }
         
-        var coin : [Coin]? = nil
+        var coin: [Coin]? = nil
         
-        do{
-            //URL로부터 data를 받아오기 전까지 기다린다.
+        do {
             let (data, _) = try await URLSession.shared.data(from: url)
             
-            //coin정보 담아서 해독할 곳.
             let coinContainer = try JSONDecoder().decode(CoinStore.self, from: data)
             coin = coinContainer.data
             
-        } catch{
+        } catch {
             print(error)
         }
         
         return coin
     }
     
-    func quoto(_ coinId : String) async -> Coin? {
+    func quote(_ coinId: String) async -> Coin? {
         
-        guard let url = URL(string: coinStoreUrl + coinId) else{
+        guard let url = URL(string: coinStoreUrl + coinId) else {
             return nil
         }
         
-        var coin : Coin? = nil
+        var coin: Coin? = nil
         
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            let coinCointainer = try JSONDecoder().decode(CoinQuery.self, from: data)
-            coin = coinCointainer.data
             
-        }catch{
+            let coinContainer = try JSONDecoder().decode(CoinQuery.self, from: data)
+            coin = coinContainer.data
+            
+        } catch {
             print(error)
         }
         
         return coin
-        
     }
 }
