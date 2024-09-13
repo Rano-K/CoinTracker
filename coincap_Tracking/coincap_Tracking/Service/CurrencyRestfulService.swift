ef568b1d7c5b66d1700aa5576e73bd4acc012aef
Rano-K <kmsoo0704@gmail.com>
2024-09-13 19:35:27 +0900

    CurrencyRestfulService 생성

--- coincap_Tracking/coincap_Tracking/Service/CurrencyRestfulService.swift
+++ coincap_Tracking/coincap_Tracking/Service/CurrencyRestfulService.swift
@@ -1,8 +1,57 @@
 //
-//  getCurrencyRestAPI.swift
+//  CurrencyRestAPI.swift
 //  coincap_Tracking
 //
 //  Created by ms k on 9/13/24.
 //
 
 import Foundation
+
+class CurrencyRestfulService {
+    private let currencyUrl = "https://api.coincap.io/v2/rates/"
+    
+    func quoteAllRates() async -> [Currency]? {
+        
+        let queryUrl = currencyUrl
+        
+        guard let url = URL(string: queryUrl) else{
+            return nil
+        }
+        
+        var currency : [Currency]? = nil
+        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let currencyContainer = try JSONDecoder().decode(CurrencyStore.self, from: data)
+            currency = currencyContainer.data
+
+        }catch{
+            print(error)
+        }
+        
+        
+        return currency
+    }
+    
+    func quote_KRW(_ currencyId: String) async -> Currency?{
+        
+        guard let url = URL(string :currencyUrl + currencyId) else { return nil
+        }
+        
+        var currency : Currency? = nil
+        do{
+            let(data, _) = try await URLSession.shared.data(from: url)
+            
+            let currencyContainer = try JSONDecoder().decode(CurrencyQuery.self, from: data)
+            currency = currencyContainer.data
+            
+            
+        }catch{
+            print(error)
+        }
+        
+        return currency
+    }
+    
+}
